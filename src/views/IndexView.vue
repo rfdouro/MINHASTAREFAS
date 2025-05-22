356935b4cd4b8b52935e443befe82655d0898977
rfdouro <rfdouro@gmail.com>
2025-05-22 14:27:27 -0300

    ajuste tab indicator 2025_05_22_14:27

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,6 +1,6 @@
 <template>
  <q-layout view="hHh lpR fFf" class="fullscreen">
-  <q-header elevated class="bg-primary text-white" height-hint="98">
+  <q-header class="bg-primary text-white" height-hint="98">
    <q-toolbar>
     <q-toolbar-title>
      <q-avatar>
